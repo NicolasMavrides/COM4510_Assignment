a22d10884a8cc4a6d36a080d09de2556d3680137
Idris Nigena <inigena1@sheffield.ac.uk>
2019-12-14 18:07:58 +0000

    Added camera and gallery functionality

--- app/src/main/java/uk/ac/shef/oak/com4510/ui/gallery/GalleryFragment.java
+++ app/src/main/java/uk/ac/shef/oak/com4510/ui/gallery/GalleryFragment.java
@@ -1,16 +1,12 @@
 package uk.ac.shef.oak.com4510.ui.gallery;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import uk.ac.shef.oak.com451.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +15,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import pl.aprilapps.easyphotopicker.DefaultCallback;
-import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class GalleryFragment extends Fragment {
     private List<ImageElement> myPictureList = new ArrayList<>();
@@ -47,81 +41,6 @@ public class GalleryFragment extends Fragment {
 
         myPictureList.add(new ImageElement(R.drawable.joe1));
 
-        // the floating button that will allow us to get the images from the Gallery
-        /*FloatingActionButton fabGallery = root.findViewById(R.id.fab_gallery);
-        fabGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EasyImage.openGallery(fragment, 0);
-            }
-        });
-
-        // the floating button that will allow us to takes pictures
-        FloatingActionButton fabCamera = root.findViewById(R.id.fab_camera);
-        fabCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EasyImage.openCamera(fragment, 0);
-            }
-        });
-        /*
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
         return root;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        EasyImage.handleActivityResult(requestCode, resultCode, data, getActivity(), new DefaultCallback() {
-            @Override
-            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
-                //Some error handling
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onImagesPicked(List<File> imageFiles, EasyImage.ImageSource source, int type) {
-                onPhotosReturned(imageFiles);
-            }
-
-            @Override
-            public void onCanceled(EasyImage.ImageSource source, int type) {
-
-            }
-        });
-    }
-
-
-    /**
-     * add the selected images to the grid
-     * @param returnedPhotos
-     */
-    private void onPhotosReturned(List<File> returnedPhotos) {
-        myPictureList.addAll(getImageElements(returnedPhotos));
-        // we tell the adapter that the data is changed and hence the grid needs
-        // refreshing
-        mAdapter.notifyDataSetChanged();
-        mRecyclerView.scrollToPosition(returnedPhotos.size() - 1);
-    }
-
-    /**
-     * given a list of photos, it creates a list of myElements
-     * @param returnedPhotos
-     * @return
-     */
-    private List<ImageElement> getImageElements(List<File> returnedPhotos) {
-        List<ImageElement> imageElementList= new ArrayList<>();
-        for (File file: returnedPhotos){
-            Log.i("File:", file.getAbsolutePath());
-            ImageElement element= new ImageElement(file);
-            imageElementList.add(element);
-        }
-        return imageElementList;
     }
 }
